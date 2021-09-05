f4da248771ad5402962bb563a0e7172009dc4d94
jfulop99 <66125323+jfulop99@users.noreply.github.com>
2021-09-05 10:30:28 +0200

    Kimaradt add module command az előző commitból

---
+++ src/main/java/com/training360/mentortools/syllabus/AddModuleCommand.java
@@ -0,0 +1,15 @@
+package com.training360.mentortools.syllabus;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddModuleCommand {
+    @NotNull
+    private Long moduleId;
+}
